01d8faab79afe44b1759a2f4cff71e4c7461e652
chriszhang <chris19980401@gmail.com>
2020-01-10 22:08:25 -0500

    new jumpsuit js added

--- show-app/src/JumpSuit/JumpSuit.js
+++ show-app/src/JumpSuit/JumpSuit.js
@@ -4,11 +4,6 @@ import Navigation from "../Navigation/Navigation"
 import "./JumpSuit.css";
 import ShowBox1 from "./ShowBox1"
 import ShowBox2 from "./ShowBox2"
-import FormalityCover from "../Images/Formality/FormalityCover.jpg"
-import Formality1 from "../Images/Formality/Formality1.jpg"
-import Formality2 from "../Images/Formality/Formality2.jpg"
-import Formality3 from "../Images/Formality/Formality3.jpg"
-import Formality4 from "../Images/Formality/Formality4.jpg"
 
 export default function JumpSuit() {
     return (
