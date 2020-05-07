432d70a19a1cc2721fa4a3b94a35c684bd7e0c85
chriszhang <chris19980401@gmail.com>
2020-05-07 15:38:23 -0400

    delete some unused import

--- show-app/src/App.js
+++ show-app/src/App.js
@@ -1,8 +1,5 @@
 import React, { Component } from "react";
 import "./App.css";
-import Navigation from "./Navigation/Navigation";
-import person_pic from "./Images/person.png"
-import { Button, Radio, Icon } from 'antd';
 import Mainview from "./RootViews/Mainview"
 
 
